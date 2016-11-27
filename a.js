c978ad333687260ef7bd04d9fbe48a19ce53c041
Zach Duerr <duerrz@oregonstate.edu>
2016-11-26 19:36:29 -0800

    added download button, styling, location to map link

--- a.js
+++
@@ -1,8 +0,0 @@
-var a = new Date('2016-11-26 19:00:00');
-
-var b = new Date('2016-11-26 18:45:00');
-var c = new Date('2016-11-26 18:03:03');
-var d = new Date('2016-11-26 16:03:03');
-var e = new Date('2016-11-26 15:03:03');
-
-console.log(a.getTime() - b.getTime());
